f3b0f4f3fb543c3068285c9575b9218ca954991c
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-11-29 14:47:10 -0500

    Changed default window title to "Unanmed"

--- Conf.lua
+++ Conf.lua
@@ -2,7 +2,8 @@
 local windowScaling = 3; -- Scales the window up by X amount
 
 function love.conf(t)
-    t.window.title = "Love2D Base"; -- Window title
+    t.window.title = "Unnamed"; -- Window title
+    
     t.window.width = 320*windowScaling;
     t.window.height = 180*windowScaling; 
 end
